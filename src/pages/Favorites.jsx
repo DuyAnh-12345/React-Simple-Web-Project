33dd22fd94e896f30e987e81161c557f9ed15206
DuyAnh-12345 <duyanh06.nguyen@gmail.com>
2026-06-04 03:15:16 +1000

    Add files via upload

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,31 @@
+
+import "../css/Favorites.css"
+import { useMovieContext } from "../contexts/MovieContext"
+import MovieCard from "../components/MovieCard"
+
+function Favorites() {
+    const {favorites} = useMovieContext();
+
+    if (favorites.length > 0) {
+        
+        return (
+        <div className ="favorites">
+        <h1>Your favorite movies</h1>
+        <div className="movies-grid">
+            {favorites.map((movie) => 
+                
+                (<MovieCard movie={movie} key={movie.id} />))}
+        </div>
+                </div>
+        )
+    }
+
+    return <div className="favorites-empty">
+        <h2>No favorite movies yet</h2>
+        <p> Start adding your favorite movie</p>
+
+
+    </div>
+}
+
+export default Favorites
